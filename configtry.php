57011798d5bdcd7e5652a2fc59bc17431b1c8e66
Yovieaji13 <71000479+Yovieaji13@users.noreply.github.com>
2021-11-29 11:04:59 +0700

    file docker

--- configtry.php
+++ configtry.php
@@ -1,13 +1,11 @@
 <?php
-    $server = "localhost";
-    $user = "root";
-    $pass = "";
-    $dbname = "dbutssister";
+    define('DB_SERVER','140.83.34.95');
+    define('DB_USER','root');
+    define('DB_PASS','Morfeen13');
+    define('DB_NAME','dbutssister');
 
-    $connect = mysqli_connect($server, $user, $pass, $dbname);
-    if ($connect) {
-    
-    } else {
-    die("Connection Failed: ".mysqli_connect_error());
+    $connect = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+    if (mysqli_connect_errorno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_errorno();
     }
 ?>
