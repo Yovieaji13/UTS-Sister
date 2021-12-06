d7eb11b5eee27223a86ebd43edc848978b6cbaca
Yovieaji13 <71000479+Yovieaji13@users.noreply.github.com>
2021-12-06 08:55:30 +0700

    coba

---
+++ coba.php
@@ -0,0 +1,26 @@
+
+<html>
+   
+   <head>
+		<title>Login | Perpustakaan</title>
+		<link href="template/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="template/css/sb-admin-2.css" rel="stylesheet" />
+		<script src="template/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="template/alert.css">
+   </head>
+   <body>
+   <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+					<div class="panel-heading">
+                        <h3 class="panel-title">Sudah Login</h3>
+                    </div>
+                    <div class="panel-body">
+					</div>
+				</div>
+			</div>
+		</div>
+	 </div>
+   </body>
+</html>
